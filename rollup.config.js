00b4b4c7782d2b11a8da9d9bc436319247b9d19e
Tobias von Klipstein <tobias@klpstn.com>
2016-09-16 12:48:21 +0200

    Export CJS

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript';
 export default {
   entry: './index.ts',
   dest: './dist/index.js',
-
+  format: 'cjs',
   plugins: [
     typescript(require('typescript'))
   ]
